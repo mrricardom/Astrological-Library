a9f1bdb6c151f6c7cb98eec128ea96d1ba948087
Ricardo Martini <Partyandhot@Ricardos-MBP.fios-router.home>
2020-07-13 16:06:50 -0400

    got API to be connected and draw data. Set result constants.

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
-console.log('checking to see if connected')
+const url = 'http://www.nokeynoshade.party/api/queens/all'
+
+const getDragQueen = async () => {
+  try {
+    const response = await axios.get(url)
+
+    const ariesQ = response.data[30]
+    const taurusQ = response.data[71]
+    const geminiQ = response.data[106]
+    const cancerQ = response.data[73]
+    const leoQ = response.data[54]
+    const virgoQ = response.data[31]
+    const libraQ = response.data[111]
+    const scorpioQ = response.data[139]
+    const sagittariusQ = response.data[82]
+    const capricornQ = response.data[44]
+    const aquariusQ = response.data[99]
+    const piscesQ = response.data[83]
+
+    console.log(piscesQ)
+  }
+  catch (error) {
+    console.log(`Your error is ${error}`)
+  }
+}
+
+getDragQueen()
